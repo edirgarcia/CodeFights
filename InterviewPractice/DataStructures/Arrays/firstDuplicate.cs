42570e531a05ed617cb5a99c206b7069982f3252
Edir Garcia Lazo <edirga@microsoft.com>
2017-07-07 16:55:34 -0700

    First Duplicate

---
+++ InterviewPractice/DataStructures/Arrays/firstDuplicate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class FirstDuplicate
+    {
+        int firstDuplicate(int[] a)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (set.Contains(a[i]))
+                {
+                    return a[i];
+                }
+                else
+                {
+                    set.Add(a[i]);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
